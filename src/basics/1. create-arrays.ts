9373a32574df217f9af6e51942339ec8d1ae5d63
Sandip Patel <sandip254@gmail.com>
2025-05-07 12:27:08 -0400

    updated create array

--- src/basics/1. create-arrays.ts
+++ src/basics/1. create-arrays.ts
@@ -1,33 +1,46 @@
-// array with primitive values
+// number type array
 const numArr: number[] = new Array(1, 2, 3);
-const booleanArr: boolean[] = [true, false, true];
+console.log("numArr", numArr);
 
-// empty array with size: n = 5
-const dynamicArray = new Array(5);
+// boolean type array
+const booleanArr: boolean[] = [true, false, true];
+console.log("booleanArr", numArr);
+
+// fixed size emptry array
+const fixedEmptyArray = new Array(5);
+console.log("fixedEmptyArray", fixedEmptyArray);
 
 // empty string array length: 0
 const emptyArray: string[] = [];
+console.log("emptyArray", emptyArray);
 
 // array with filled values or elements
-const filledArray = new Array(3).fill(0); // [0, 0, 0]
+const filledArray = new Array(3).fill(0);
+console.log("filledArray", filledArray);
 
 // tuples: mixed array
 const mixedArray: (string | number | boolean)[] = ["text", 42, true];
+console.log("mixedArray", mixedArray);
 
 // array with iterable
-const fromIterable = Array.from([1, 2, 2, 3]); // [1, 2, 3]
+const fromIterable = Array.from([1, 2, 2, 3]);
+console.log("fromIterable", fromIterable);
 
 // array from json values
 const jsonArray = JSON.parse('[1, "two", false]');
+console.log("jsonArray", jsonArray);
 
 // array from object
 const entriesArray = Array.from(Object.entries({ a: 1, b: 2 }));
+console.log("entriesArray", entriesArray);
 
 // multi dimentional array
 const matrix: number[][] = [
   [1, 2],
   [3, 4],
 ];
+console.log("matrix", matrix);
 
 // array using spread operator
-const spreadArray = [...[1, 2], ...[3, 4]]; // [1,2,3,4]
+const spreadArray = [...[1, 2], ...[3, 4]];
+console.log("spreadArray", spreadArray);
